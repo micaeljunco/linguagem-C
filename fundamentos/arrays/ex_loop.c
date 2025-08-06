32212fb3765f11ceb0c09975396a5345daa2bddc
Micael Junco <micael.j.junco@gmail.com>
2025-08-06 13:02:35 -0300

    exemplo de loop com array
    esse é um loop que percorre todos os valores de um array.

---
+++ fundamentos/arrays/ex_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int arrayParaIterar[] = {
+        20, 56, 21, 40, 10
+    };
+    int i;
+
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d\n", arrayParaIterar[i]);
+    }
+    
+
+    return 0;
+}
