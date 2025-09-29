473410c662b0cf6f347cf0931554ba814af3ca94
Micael Junco <micael.j.junco@gmail.com>
2025-09-29 17:09:36 -0300

    inacabado

--- exercicios-senai/ex_m_infantil.c
+++ exercicios-senai/ex_m_infantil.c
@@ -3,6 +3,27 @@
 int main()
 {
     int n_criancas;
+
+    printf("Insira o número de crianças nascidas no período:");
+    scanf("%d%*c",&n_criancas);
+
+    if (n_criancas <= 0)
+    {
+        printf("Valor inválido.\n");
+        return 1;
+    }
     
+    char cri_sexo[n_criancas];
+    char cri_tempo_vida[n_criancas];
+
+    for (size_t i = 0; i < n_criancas; i++)
+    {
+        printf("Sexo da %dª criança:",i+1);
+        scanf("%c", &cri_sexo[i]);
+        
+        printf("Tempo de vida da %dª criança:",i+1);
+        scanf("%c", &cri_tempo_vida[i]);
+    }
+
     return 0;
 }
